6a6cb6c17ea7095cb49edaa8ae46caefbe309793
Manipradeep Marichamy <manipradeep_marichamy@epam.com>
2022-02-18 16:22:34 +0530

    spring  web socket

--- spring-web-socket/src/main/java/com/websocket/sample/repository/StudentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.websocket.sample.repository;
-
-import com.websocket.sample.entity.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<Student, Long> {
-}
---
+++ spring-web-socket/src/test/java/com/websocket/sample/service/SocketTextHandler.java
@@ -0,0 +1,21 @@
+package com.websocket.sample.service;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import java.io.IOException;
+
+@Component
+public class SocketTextHandler extends TextWebSocketHandler{
+    @Override
+    public void handleTextMessage(WebSocketSession session, TextMessage message)
+            throws InterruptedException, IOException, JSONException {
+
+        String payload = message.getPayload();
+        JSONObject jsonObject = new JSONObject(payload);
+        session.sendMessage(new TextMessage("Hi " + jsonObject.get("user") + " how may we help you?"));
+    }
+}
