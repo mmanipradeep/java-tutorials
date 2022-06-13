76f50b0bbd48122f0ccb4fee09204b18594d0d52
Manipradeep Marichamy <manipradeep_marichamy@epam.com>
2022-06-13 19:29:48 +0530

    adding missed files

---
+++ interview_preparation/Frontend/html/hide-view-source-html.js
@@ -0,0 +1,11 @@
+document.addEventListener("keydown", function (event){
+    if (event.ctrlKey){
+       event.preventDefault();
+    }
+    if(event.keyCode == 123){
+       event.preventDefault();
+    }
+});
+
+//in script tags
+// No Follow  , No index
